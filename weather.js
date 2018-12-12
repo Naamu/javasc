0a664dbc87875fd3edcd463b57788d5da5c09022
Naamu <45629631+Naamu@users.noreply.github.com>
2018-12-12 23:16:05 +0900

    location part1

---
+++ weather.js
@@ -0,0 +1,42 @@
+
+const COORDS = 'coords';
+
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,  //longitude : longitude 와 같음.
+        longitude
+    };
+    saveCoords(coordsObj);
+
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        //getWeather
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
